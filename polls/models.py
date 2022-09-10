01c363c6804c6c46d2170f371419fb16c2b6ac42
Rudreshcg <rudresha.gangaswamaiah@softsuave.com>
2022-09-10 13:24:01 +0530

    adding features

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class Question(models.Model):
+    question_txt = models.CharField(max_length=200)
+    pub_date = models.DateField()
+
+    def __str__(self):
+        return self.question_txt
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_txt = models.CharField(max_length=200)
+    vote = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_txt
